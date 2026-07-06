b11a6f6b5a1ccdab7726085efec5c554a4f5eefc
Givanilson Geferson Fernandes Mariz <givanilson.fernandes@academico.ifrn.edu.br>
2026-07-06 08:23:00 -0300

    sistema funcional de pesquisa

--- ressaca-lit-front/app/pesquisar/page.tsx
+++ ressaca-lit-front/app/pesquisar/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Playfair_Display } from "next/font/google"
 import styles from "@/app/styles/Pesquisa.module.css"
 import axios from "axios"
 import Image from "next/image"
@@ -14,6 +15,10 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props"
 //         return '/imagens/naoencontrado.png'
 //     }
 // }
+
+const playfair = Playfair_Display({
+    subsets: ['latin']
+})
 
 export default function Page() {
     const [estado, setEstado] = useState('nada')
@@ -32,7 +37,7 @@ export default function Page() {
             setLivros([])
             e.preventDefault()
             const dados = new FormData(e.currentTarget)
-            const res = await axios.get(`https://openlibrary.org/search.json?q=${dados.get('query')}`)
+            const res = await axios.get(`https://openlibrary.org/search.json?q=${dados.get('query')}&lang=pt`)
             setLivros(res.data.docs)
         } catch(e) {
             console.log(e)
@@ -41,7 +46,7 @@ export default function Page() {
     }
     return (
         <div>
-            <header className={`${styles.barra_de_pesquisa} flex justify-center h-[5rem] items-center sticky`}>
+            <header className={`${styles.barra_de_pesquisa} flex justify-center h-[5rem] items-center `}>
                 <form onSubmit={handleSubmit} className="flex flex-row justify-center items-center w-[70vw] gap-6 bg-white h-1/2 p-2 rounded-full font-sans">
                     <input type="text" name="query" className="w-full focus:outline-0" placeholder="Insira o nome do livro" />
                     <button type="submit" className="text-2xl cursor-pointer">
@@ -55,16 +60,23 @@ export default function Page() {
                 </form>
             </header>
             <main>
-                <div className="flex flex-row flex-wrap gap-20 font-sans min-h-[100vh] min-w-[100vw] justify-center items-center pt-5">
-                    {livros.length != 0 && livros.map((livro: any, index)=> (
-                        <div key={livro.key} className="h-[35rem] w-[28rem] flex flex-col flex-grow-0 p-4 bg-gray-100 rounded-xl">
-                            <span className="font-bold h-[3rem]">{livro.title}</span>
-                            <Image className="self-center justify-self-center max-h-[25rem] bg-gray-200 p-2 rounded-xl min-h-[25rem]"
+                <div className="flex flex-row flex-wrap gap-20 font-sans min-h-[90vh] max-h-91 min-w-[100vw] justify-center items-center pt-5 overflow-scroll">
+                    {livros.length != 0 && livros.map((livro: Livro, index)=> (
+                        <div key={livro.key} className={`h-[25rem] w-[22rem] flex flex-col flex-grow-0 p-4 bg-gray-100 rounded text-lg ${playfair.className} overflow-scroll`}>
+                            {livro.title.length <= 70 && <span className="font-bold h-[4rem] mb-3 border-b-1 border-black">{livro.title}</span>}
+                            {livro.title.length > 70 && <span className="font-bold h-[4rem] mb-3 border-b-1 border-black">{livro.title.slice(0,60)}...</span>}
+                            <Image className=" justify-self-center max-h-[10rem] bg-gray-200  rounded-xl min-h-[10rem]"
                                 src={getImage(livro)}
                                 alt="Não encontrado"
-                                width={400}
+                                width={120}
                                 height={250}
                             />
+                            {livro.author_name && livro.author_name.length > 1 &&   
+                            <span className="font-bold mt-3 border-t-1 border-black">Autores: {livro.author_name.map((autor, index) => (
+                                <span key={index}><span>{autor}</span> <br /></span>
+                            ))}</span>
+                            }
+                            {livro.author_name && livro.author_name.length == 1 && <span className="font-bold mt-3 border-t-1 border-black">Autor: {livro.author_name}</span>}
                         </div>
                     ))}
                     {estado == 'carregando' && <span className="">Carregando...</span>}
